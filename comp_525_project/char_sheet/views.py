0bfd9af32b8df99ce0db739a7e9adb505b2ecd78
John Smith <nbwcoc@openmailbox.org>
2016-11-11 19:16:48 -0500

    add JSON interface for characters. Accessed at char/dump?cid=x where x is the character's id in the database.

--- comp_525_project/char_sheet/views.py
+++ comp_525_project/char_sheet/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from . import models
+from . import encoder
 
 # Create your views here.
+
+def dump(request):
+    if not "cid" in request.GET:
+        return JsonResponse({})
+    return JsonResponse(models.Character.objects.filter(id=request.GET["cid"])[0],
+                        encoder=encoder.CharacterEncoder, safe=False)
+
