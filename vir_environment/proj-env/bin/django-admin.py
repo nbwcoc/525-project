7afc8012e1eaad6283a43ccae26089661d6f849c
Wesley <wmcouturier@gmail.com>
2016-11-14 23:18:45 -0500

    Virtual Environment Setup & updated Project Proposal
    use command "source proj-env/bin/activate" from project envir. to activate it

---
+++ vir_environment/proj-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wcouturier/UNHWork/COMP525/GithubRepo/525-project/vir_environment/proj-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
