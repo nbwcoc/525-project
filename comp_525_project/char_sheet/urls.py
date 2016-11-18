deade4ed9406944592af9b4546bb03850b50562a
John Smith <nbwcoc@openmailbox.org>
2016-11-18 16:32:10 -0500

    Added view to update/add information in the database at char/update/. This is a POST interface. See the docstring for details. Also added a view to test it at char/testpost/

--- comp_525_project/char_sheet/urls.py
+++ comp_525_project/char_sheet/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^dump/', views.dump, name="dump"),
-    url(r'view_char/', views.view_char, name='view_char'),
+    url(r'^view_char/', views.view_char, name='view_char'),
+    url(r'^update/', views.update, name="update"),
+    url(r'^testpost/', views.test_post, name="test_post")
 ]
--- comp_525_project/char_sheet/views.py
+++ comp_525_project/char_sheet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
+import json
 from . import models
 from . import encoder
 
@@ -97,3 +98,82 @@ def view_char(request):
                "classes": player_classes, "races": races,
                "backgrounds": backgrounds, "level": level}
     return render(request, "char/view_char.html", context)
+
+def update(request):
+    """
+    Updates information to the database
+    The request should contain JSON data in the same form as what is returned
+    by dump, with an additional key "cid" to indicate the id of the character
+    in the database being updated.
+
+    Right now, there is no good reason to update any other values in the
+    database.
+
+    The ID is the only mandatory field.
+
+    Only one ID can be used at once.
+    An ID of zero will be interpreted as inserting into the table.
+
+    This can be sent either as the sole contents of the request body, or in
+    a key named "data".
+
+    Returns an HTTP response code of 400 on a malformed request and 200 on a
+    success with the item's id in the body.
+    """
+    if request.method != "POST":
+        return HttpResponse(status=400)
+
+    if len(request.POST) > 0:
+        if not "data" in request.POST:
+            return HttpResponse(status=400)
+        data = json.loads(request.POST["data"])
+    else:
+        data = json.loads(request.body.decode("utf-8"))
+
+    if "cid" in data:
+        data["cid"] = int(data["cid"])
+        if data["cid"] == 0:
+            new_item = models.Character()
+        else:
+            new_item = models.Character.objects.filter(id=data["cid"])[0]
+    # I don't think there's any reason to update this data from here
+    # elif "rid" in data:
+    #     data["rid"] = int(data["rid"])
+    #     if data["rid"] == 0:
+    #         new_item = models.Race()
+    #     else:
+    #         new_item = models.Race.objects.filter(id=data["rid"])[0]
+    # elif "bid" in data:
+    #     data["bid"] = int(data["bid"])
+    #     if data["bid"] == 0:
+    #         new_item = models.Background()
+    #     else:
+    #         new_item = models.Background.objects.filter(id=data["bid"])[0]
+    # elif "pcid" in data:
+    #     data["pcid"] = int(data["pcid"])
+    #     if data["pcid"] == 0:
+    #         new_item = models.PlayerClass()
+    #     else:
+    #         new_item = models.PlayerClass.objects.filter(id=data["pcid"])[0]
+    else:
+        print("DEBUG: invalid ID")
+        return HttpResponse(status=400)
+
+    for key in data:
+        if not hasattr(new_item, key):
+            if key in ["cid", "rid", "bid", "pcid"]:
+                continue
+            return HttpResponse(status=400)
+        if key == "background":
+            new_item.background = models.Background.objects.filter(id=data[key])[0]
+        elif key == "race":
+            new_item.race = models.Race.objects.filter(id=data[key])[0]
+        elif key == "player_class":
+            new_item.player_class = models.PlayerClass.objects.filter(id=data[key])[0]
+        else:
+            setattr(new_item, key, data[key])
+    new_item.save()
+    return HttpResponse(new_item.id, status=200)
+
+def test_post(request):
+    return render(request, "char/testpost.html", {})
