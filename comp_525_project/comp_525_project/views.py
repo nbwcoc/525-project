cb8bf099f4855055f2aa5dad76e6b0992f35e6a0
John Smith <nbwcoc@openmailbox.org>
2016-12-05 20:24:43 -0500

    oops

---
+++ comp_525_project/comp_525_project/views.py
@@ -0,0 +1,17 @@
+"""
+auth
+"""
+
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+def logon(request):
+    try:
+        user = authenticate(request.POST["username"], request.POST["password"])
+    except PermissionDenied:
+        return HttpResponse(status=403)
+    if user is None:
+        return HttpResponse(status=403)
+    else:
+        login(request, user)
+        return HttpResponse(user.id, status=200)
